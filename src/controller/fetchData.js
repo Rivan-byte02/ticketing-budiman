d48453d173dc6211ab39e931fb0110dba8a975c5
Kelompok30 <rivanma@Rivans-MacBook-Pro.local>
2023-04-11 05:22:46 +0700

    add print func

--- src/controller/fetchData.js
+++ src/controller/fetchData.js
@@ -2,19 +2,22 @@ import axios from "axios";
 
 const fetchTicketData = async (bookingNumber) => {
   try {
-    const { data } = await axios.get(
-      `https://dev.budiman.io/api/v2/search-ticket/cetak-tiket/${bookingNumber}`,
+    const { data } = await axios.post(
+      `https://dev.budiman.io/api/v2/search-ticket/cetak-tiket/invoice`,
+      {
+        invoice: bookingNumber,
+      },
       {
         headers: {
           Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjgzMDc2ODg4LCJpYXQiOjE2ODA0ODQ4ODgsImp0aSI6IjFlODMyMTY0MjFmMzQzMmI5YTAxOTVkNzdiMmU5YzMwIiwidXNlcl9pZCI6MTExNTR9.pK3s6sQac9Lg5auSCbDAUkVTc6-s9H_-meeaXl5ek7E`,
         },
       }
     );
-    console.log(data);
-    return data;
+    console.log(data.list_ticket);
+    return data.list_ticket;
   } catch (error) {
     console.log(error);
-    return {};
+    return [];
   }
 };
 
--- src/view/ticketSearching/index.js
+++ src/view/ticketSearching/index.js
@@ -1,138 +1,139 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { fetchTicketData } from "../../controller/fetchData";
+import { useDebounce } from "use-debounce";
 import "./index.scss";
 
 function TicketSearching() {
   const [bookingCode, setBookingCode] = useState("");
-  const [errorFetch, setErrorFetch] = useState(false);
-  const [ticketData, setTicketData] = useState({});
+  const [currentBookingCode, setCurrentBookingCode] = useState("");
+  const [value] = useDebounce(bookingCode, 1000);
+  const [ticketData, setTicketData] = useState([]);
 
-  const rupiah = (number)=>{
+  const rupiah = (number) => {
     return new Intl.NumberFormat("id-ID", {
       style: "currency",
-      currency: "IDR"
+      currency: "IDR",
     }).format(number);
-  }
+  };
+
+  const fetchTicketWithInvoice = async (e) => {
+    const result = await fetchTicketData(e);
+    if (result.length) {
+      setTicketData(result);
+      setTimeout(() => {
+        window.print()
+      }, 1000)
+    }
+  };
+
+  useEffect(() => {
+    if (value.length && currentBookingCode.length) {
+      fetchTicketWithInvoice(value);
+    }
+  }, [value, currentBookingCode.length]);
 
   return (
-    <div className="search-container">
-      {Object.keys(ticketData).length ? (
-        <div className="ticket-card">
-          <header>
-            <div>
-              <h2>{ticketData.code_tiket}</h2>
-              <h4>{ticketData.invoice}</h4>
+    <div className={`search-container${ticketData.length ? " ticket-list" : ""}`}>
+      {ticketData.length ? (
+        ticketData.map((e, i) => {
+          return (
+            <div className="ticket-card" key={i}>
+              <header>
+                <div>
+                  <h2>{e.code_tiket}</h2>
+                  <h4>{e.invoice}</h4>
+                </div>
+                <img src={e.qr_code} alt="qr-code" />
+              </header>
+              <div className="divider-container">
+                <div className="circle"></div>
+                <div className="divider"></div>
+                <div className="circle right"></div>
+              </div>
+              <section>
+                <section>
+                  <h3 className="title">Nama Penumpang</h3>
+                  <h3>{e.nama_penumpang}</h3>
+                </section>
+                <section>
+                  <h3 className="title">Telepon Penumpang</h3>
+                  <h3>{e.no_tlp_penumpang}</h3>
+                </section>
+                <section>
+                  <h3 className="title">Tanggal Cetak</h3>
+                  <h3>{e.tgl_cetak}</h3>
+                </section>
+                <section>
+                  <h3 className="title">Tanggal Transaksi</h3>
+                  <h3>{e.tgl_transaksi}</h3>
+                </section>
+                <div className="divider-container">
+                  <div className="circle"></div>
+                  <div className="divider"></div>
+                  <div className="circle right"></div>
+                </div>
+                <section>
+                  <h3 className="title">Cetak Oleh</h3>
+                  <h3>{e.cetak_oleh}</h3>
+                </section>
+                <section className="row">
+                  <section>
+                    <h3 className="title">Nama Agen</h3>
+                    <h3>{e.agen_name}</h3>
+                  </section>
+                  <section>
+                    <h3 className="title">Kode Agen</h3>
+                    <h3>{e.agen_code}</h3>
+                  </section>
+                </section>
+                <section className="row">
+                  <section>
+                    <h3 className="title">Berangkat</h3>
+                    <h3>{e.berangkat}</h3>
+                  </section>
+                  <section>
+                    <h3 className="title">Tujuan</h3>
+                    <h3>{e.tujuan}</h3>
+                  </section>
+                </section>
+                <section className="row">
+                  <section>
+                    <h3 className="title">Nomor Kursi</h3>
+                    <h3>{e.no_kursi}</h3>
+                  </section>
+                  <section>
+                    <h3 className="title">Jam Keberangkatan</h3>
+                    <h3>{e.jam_berangkat}</h3>
+                  </section>
+                </section>
+                <section className="row">
+                  <section>
+                    <h3 className="title">Harga Tiket</h3>
+                    <h3>{rupiah(e.harga_tiket)}</h3>
+                  </section>
+                  <section>
+                    <h3 className="title">Tipe Bus</h3>
+                    <h3>{e.tipe_bus}</h3>
+                  </section>
+                </section>
+              </section>
             </div>
-            <img src={ticketData.qr_code} />
-          </header>
-          <div className="divider-container">
-            <div className="circle"></div>
-            <div className="divider"></div>
-            <div className="circle right"></div>
-          </div>
-          <section>
-            <section>
-              <h3 className="title">Nama Penumpang</h3>
-              <h3>{ticketData.nama_penumpang}</h3>
-            </section>
-            <section>
-              <h3 className="title">Telepon Penumpang</h3>
-              <h3>{ticketData.no_tlp_penumpang}</h3>
-            </section>
-            <section>
-              <h3 className="title">Tanggal Cetak</h3>
-              <h3>{ticketData.tgl_cetak}</h3>
-            </section>
-            <section>
-              <h3 className="title">Tanggal Transaksi</h3>
-              <h3>{ticketData.tgl_transaksi}</h3>
-            </section>
-            <div className="divider-container">
-              <div className="circle"></div>
-              <div className="divider"></div>
-              <div className="circle right"></div>
-            </div>
-            <section>
-              <h3 className="title">Cetak Oleh</h3>
-              <h3>{ticketData.cetak_oleh}</h3>
-            </section>
-            <section className="row">
-              <section>
-                <h3 className="title">Nama Agen</h3>
-                <h3>{ticketData.agen_name}</h3>
-              </section>
-              <section>
-                <h3 className="title">Kode Agen</h3>
-                <h3>{ticketData.agen_code}</h3>
-              </section>
-            </section>
-            <section className="row">
-              <section>
-                <h3 className="title">Berangkat</h3>
-                <h3>{ticketData.berangkat}</h3>
-              </section>
-              <section>
-                <h3 className="title">Tujuan</h3>
-                <h3>{ticketData.tujuan}</h3>
-              </section>
-            </section>
-            <section className="row">
-              <section>
-                <h3 className="title">Nomor Kursi</h3>
-                <h3>{ticketData.no_kursi}</h3>
-              </section>
-              <section>
-                <h3 className="title">Jam Keberangkatan</h3>
-                <h3>{ticketData.jam_berangkat}</h3>
-              </section>
-            </section>
-            <section className="row">
-              <section>
-                <h3 className="title">Harga Tiket</h3>
-                <h3>{rupiah(ticketData.harga_tiket)}</h3>
-              </section>
-              <section>
-                <h3 className="title">Tipe Bus</h3>
-                <h3>{ticketData.tipe_bus}</h3>
-              </section>
-            </section>
-          </section>
-        </div>
+          );
+        })
       ) : (
         <div className="search-card">
           <header>
-            <h2>Masukkan Kode Booking / Pembayaran</h2>
-            <h4>Enter Book Code / Payment Code</h4>
+            <h2>Scan QR Code di aplikasi Budiman Mobile</h2>
           </header>
           <input
-            onChange={(e) => {
+            onChange={async (e) => {
               setBookingCode(e.target.value);
+              setCurrentBookingCode(e.target.value);
             }}
             type={"text"}
-            placeholder={"Contoh: T130822190921"}
+            placeholder={"Contoh : INV20230402BDMN"}
           />
-          {errorFetch && <h4>Tolong masukan Kode Booking</h4>}
-          <div className="button-container">
-            <div
-              onClick={async () => {
-                if (!bookingCode.trim().length) {
-                  setErrorFetch(true);
-                  return;
-                }
-                setErrorFetch(false);
-                const result = await fetchTicketData(bookingCode);
-                setTicketData(result);
-              }}
-              className="button"
-            >
-              <span class="material-symbols-outlined">search</span>
-              <div>
-                <h5>Cari Tiket Saya</h5>
-                <h6>Search My Ticket</h6>
-              </div>
-              <span class="material-symbols-outlined">arrow_forward</span>
-            </div>
-          </div>
+          <img src={require("../../logo-budiman.png")} alt="budiman-logo" />
         </div>
       )}
     </div>
